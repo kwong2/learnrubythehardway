89c21c56733f6f762025d05a9ab9ed78bb7ebae3
Kevin Wong <k.wong4@yahoo.com>
2017-07-07 21:46:02 -0400

    Exercise 1 added

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not tough this.'
+
+#puts "Look at me,look at me. I'm the Captain now."
